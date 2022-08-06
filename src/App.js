8d68ab8e7313492d6611c703de811b49806d4ccc
Ruslan Aliyev <ruslan@scaleflex.com>
2022-08-06 13:10:56 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import Pagination from "react-js-pagination";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// https://www.npmjs.com/package/react-js-pagination
+// https://medium.com/@kumarkaushik1580/pagination-in-react-js-using-react-js-pagination-library-689668b4b9db
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const recordPerPage = 10;
+  const totalRecords = 40;
+
+  const handlePageChange = (pageNumber) => {
+    console.log(pageNumber);
+    // Call AJAX
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='page-pagination'>
+        <Pagination
+          prevPageText='Prev'
+          nextPageText='Next'
+          firstPageText='First'
+          lastPageText='Last'
+          activePage={currentPage}
+          itemsCountPerPage={recordPerPage}
+          totalItemsCount={totalRecords}
+          onChange={handlePageChange}
+          itemClass="page-item"
+          linkClass="page-link"
+        />
+      </div>
     </div>
   );
 }
